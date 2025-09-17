c6fcea5b1c694c8afa61f5dace4793fe918195b4
santyasm <yyasant5@gmail.com>
2025-09-16 21:33:55 -0300

    feat(auth): add register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,33 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $credentials = $request->validate([
-            "email" => "required|email",
-            "password" => "required"
-        ]);
+        try {
+            $credentials = $request->validate([
+                "email" => "required|email",
+                "password" => "required"
+            ]);
 
 
-        if (!Auth::attempt($credentials)) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            if (!Auth::attempt($credentials)) {
+                return response()->json(['message' => 'Unauthorized'], 401);
+            }
+
+            $user = $request->user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ]
+            ]);
+        } catch (\Exception $ex) {
+            return response()->json(['message' => $ex->getMessage()], 500);
         }
+    }
 
-        $user = $request->user();
-        $token = $user->createToken('auth_token')->plainTextToken;
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                "name" => "required|string|max:255",
+                "email" => "required|email|unique:users",
+                "password" => "required|string|min:8"
+            ]);
 
-        return response()->json([
-            'token' => $token,
-            'user' => [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-            ]
-        ]);
+            $user = User::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "password" => $request->password,
+            ]);
+
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ],
+            ], 201);
+        } catch (ValidationException $e) {
+
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred during registration.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::get('/user', function (Request $request) {
+Route::get('/users', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
