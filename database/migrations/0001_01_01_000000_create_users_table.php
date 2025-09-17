3bc9c33f72fdf414c933d26a84653c891bc7911b
santyasm <yyasant5@gmail.com>
2025-09-16 22:45:33 -0300

    add role enum to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['student', 'admin'])->default('student');
+            $table->enum('role', ['student', 'admin', 'instructor'])->default('student');
             $table->rememberToken();
             $table->timestamps();
         });
