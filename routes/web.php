ac8669f0479e9669aa5402118f8ceee7e7dd7f5d
santyasm <yyasant5@gmail.com>
2025-10-16 19:25:36 -0300

    chore: update Caddyfile to add trusted proxies configuration and revert root route to return welcome view, improving request handling and server settings

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
-Route::get('/', function (Request $request) {
-    // return view('welcome');
-    dd(
-        'Request is Secure?', $request->isSecure(),
-        'X-Forwarded-Proto Header:', $request->header('x-forwarded-proto'),
-        'All Headers:', $request->headers->all()
-    );
+Route::get('/', function () {
+    return view('welcome');
 });
 
 Route::get('/up', function () {
