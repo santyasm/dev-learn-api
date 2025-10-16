7d2352b5c86e0583dab6ec9ea3a70f0f51fe5cd8
santyasm <yyasant5@gmail.com>
2025-10-16 19:17:49 -0300

    fix: modify root route to debug request details, replacing view with request inspection for development purposes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    dd(
+        'Request is Secure?', $request->isSecure(),
+        'X-Forwarded-Proto Header:', $request->header('x-forwarded-proto'),
+        'All Headers:', $request->headers->all()
+    );
 });
 
 Route::get('/up', function () {
